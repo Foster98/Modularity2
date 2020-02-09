e04b5e93bb441fef2b5fa82e239ca7473b2fd5dd
haden <hgfoster@bsu.edu>
2020-02-09 11:36:00 -0500

    new Selection sort, selection sort test, exporter, and provider

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,36 @@
-import exporters.HTMLExporter;
-import exporters.NumberExporter;
-import providers.FromFileNumberProvider;
-import providers.NumberProvider;
-import providers.RandomNumberProvider;
-import sorters.BubbleSorter;
-import sorters.NumberSorter;
 
-import java.util.Collections;
+import exporters.ConExporter;
+
+import exporters.NumberExporter;
+
+import providers.NumberProvider;
+
+
+
+import providers.UserProvider;
+
+import sorters.NumberSorter;
+import sorters.SelectionSorter;
+
+
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        NumberProvider theProvider = new RandomNumberProvider(System.nanoTime());
+
+
+        NumberProvider theProvider = new UserProvider();
 
         var theNumbers = theProvider.provideNumbers();
 
         System.out.println("BEFORE SORTING: [" + theNumbers.stream().map(Object::toString).collect(Collectors.joining(",")) + "]");
 
-        NumberSorter theSorter = new BubbleSorter();
+        NumberSorter theSorter = new SelectionSorter();   ///new sorter goes here HERE
         var sortedNumbers = theSorter.sort(theNumbers);
 
         System.out.println("AFTER SORTING : [" + sortedNumbers.stream().map(Object::toString).collect(Collectors.joining(",")) + "]");
 
-        NumberExporter theExporter = new HTMLExporter("numbersFormatted");
+        NumberExporter theExporter = new ConExporter();//To HERE
         theExporter.export(sortedNumbers);
     }
 }
